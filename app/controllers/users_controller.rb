e2b57ec0abcaf93ce884116d07ecd233bf4e5c92
Nia Bert <nbert14@gmail.com>
2019-09-06 01:24:44 +0000

    add user login methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,24 @@ class UsersController < ApplicationController
   #creating a key value pair to the session hash (creating a session)
   #the purpose of this route is to receive the login form, find the use and log the user in
   post '/login' do
+    #find user 
+    @user = User.find_by(username: params[:username])
+    #authenticate user 
+    if @user.authenticate(params[:password])
+    #log user in - create user session
+    session[:user_id] = @user.id
+    #redirect to user
+    redirect "users/#{@user.id}"
+  else 
+    #error message
+    #redirect to log in page 
   end
   #sign up?
   
-  get '/signup'
+  get '/signup' do
+  end
+  
+  get '/users/:id' do
+    "user show route"
+  end
 end
