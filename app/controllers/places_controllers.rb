ec240a64e9a96f185d8561660b8eca844f729f09
Nia Bert <nbert14@gmail.com>
2019-09-09 02:54:03 +0000

    created new places with helper methods

--- app/controllers/places_controllers.rb
+++ app/controllers/places_controllers.rb
@@ -6,7 +6,21 @@ class PlacesController < ApplicationController
   
   #post places to create new places
   
+  post '/places' do
+    #want to create new place and save it to the database but only if it has content
+    #and only if user is logged in 
+    if !logged_in?
+      redirect '/'
+    end
+    if params[:city_name] !=""
+      @place = Place.create(city_name: params[:city_name], user_id: current_user.id)
+      redirect "/places/#{@place.id}"
+    else
+      redirect '/places/new'
+  end
+  
   #show route for place
+  
   
   #index route for all places
 end
